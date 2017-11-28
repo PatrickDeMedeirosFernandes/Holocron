d4fe168ca4045774e5cfe57078982d319c829e41
Patrick <Patrick@192.168.246.59>
2017-11-28 17:46:39 -0200

    envio de perguntas por csv

--- Admin/CadLotePerg.php
+++ Admin/CadLotePerg.php
@@ -9,8 +9,53 @@
         </div>
         <!-- /.col-lg-12 -->
     </div>
-    <!-- /.row -->
-    
+        <!-- /.panel-heading -->
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="table-responsive">
+                        <h2>Adcionar arquivo CSV</h2>
+                        <form class="form-group"  enctype="multipart/form-data"
+                              name="myForm" id="myForm"  action="admin/controller/CSVADC.php" method="POST">
+                            <input type="file" name="userfile"  class="btn btn-block btn-default">
+                            <input type="submit" class="btn btn-block btn-success" value="enviar" /> 
+                        </form>
+                        <P>
+                            <B>NOTA PARA O ENVIO DE ARQUIVO:</B>
+                            <BR>
+                            Para o enivio arquivos para o cadastro em massa no sistema, é necessário seguir oo arquivo .csv
+                            <br>
+                            ser arquivo tem que ser salvo em CSV separado por ponto e virgula ou virgulas.
+                        </P>
+                    </div>
+                    <!-- /.table-responsive -->
+                    <H4>Lista de Arquivos no sistema:</H4>
+                    <?php
+                    $pasta = '../admin/ANEXO/';
+
+                    if (is_dir($pasta)) {
+                        $diretorio = dir($pasta);
+
+                        while (($arquivo = $diretorio->read()) !== false) {
+                            if ($arquivo != "." && $arquivo != "..") {
+                                echo '<br><a href="admin/ANEXO/' . $arquivo . '">' . $arquivo . '</a><br />';
+                                $arquivo2 = fopen("../admin/ANEXO/" . $arquivo, "r");
+//                            // Lê o conteúdo do arquivo
+                            }
+                        }
+
+                        $diretorio->close();
+                    } else {
+                        echo 'A pasta não existe.';
+                    }
+                    ?>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.panel-body -->
+    </div>
+</div>
     <!-- /.row -->
 </div>
 <!-- /#page-wrapper -->
---
+++ Admin/controller/CSVADC.php
@@ -0,0 +1,96 @@
+<?php
+//$uploaddir = "../ANEXO/";
+//OCULTA OS ERROS :V
+//ini_set('display_errors', 0 );
+//error_reporting(0);
+
+$target_dir = "../ANEXO/";
+$target_file = $target_dir . basename($_FILES["userfile"]["name"]);
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+
+$check = getimagesize($_FILES["userfile"]["tmp_name"]);
+
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    ?><script>
+        alert('ARQUIVO JÁ EXISTI');
+   window.location.href = '../../ADM';
+    </script>
+    <?PHP
+} else {
+// Check file size
+    if ($_FILES["userfile"]["size"] < 5) {
+        ?><script>
+            alert('ARQUIVO COM TAMANHO NÁO SUPORTADO');
+              window.location.href = '../../ADM';
+        </script>
+        <?PHP
+    } else {
+// Allow certain file formats
+        if ($imageFileType != "csv" && $imageFileType != "txt") {
+            ?><script>
+                alert('ARQUIVO TEM QUE SER .CSV OU .TXT');
+   window.location.href = '../../ADM';
+            </script>
+            <?PHP
+        } ELSE {
+// Check if $uploadOk is set to 0 by an error
+            if (move_uploaded_file($_FILES["userfile"]["tmp_name"], $target_file)) {
+                //        echo "The file " . basename($_FILES["userfile"]["name"]) . " has been uploaded.<br>";
+//                echo 'Aqui está mais informações de debug:';
+                //   print_r($_FILES);
+                include_once '../../controller/DB.php';
+
+                $arquivo = fopen("../ANEXO/" . $_FILES['userfile']['name'], "r");
+                //    $arquivo = fopen('dados_emails.csv', 'r');
+                // Lê o conteúdo do arquivo
+                while (!feof($arquivo)) {
+                    // Pega os dados da linha
+                    $linha = fgets($arquivo, 1024);
+                    // Divide as Informações das celular para poder salvar
+                    $dados = str_replace(':', ';', $linha);
+                    $dados = str_replace(',', ';', $linha);
+                    $dados = explode(';', $linha);
+                    //  // Verifica se o Dados Não é o cabeçalho ou não esta em branco
+                    if (!empty($linha)) {
+                        $sql = " INSERT INTO `respota`(`resposta`) VALUES ("
+                                . "'" . utf8_encode($dados[1]) . "');";
+                        //  echo $sql;
+                        $result = mysqli_query($conn, $sql);
+                        $sql3 = "SELECT id FROM `respota` ORDER BY id DESC LIMIT 1";
+                        //             echo $sql3;
+                        $result2 = mysqli_query($conn, $sql3);
+
+                        while ($row = mysqli_fetch_assoc($result2)) {
+                            $resp = $row["id"];
+                        }
+
+
+                        echo $resp;
+                        $sql2 = " INSERT INTO `pergunta`(`pergunta`, `valida`, `resposta_pergunta`) VALUES ("
+                                . "'" . utf8_encode($dados[0]) . "',1,$resp
+
+                              );";
+                        $result4 = mysqli_query($conn, $sql2);
+                        echo $sql2;
+                    }
+                    ?><script>
+                        alert('Envio de dados terminado');
+                                 window.location.href = '../../ADM';
+
+                    </script>
+                    <?PHP
+                }
+            } else {
+                ?><script>
+                    alert('Aconteceu algum problema no envio do arquivo e náo foi enviado');
+                   window.location.href = '../../ADM';
+                </script>
+                <?PHP
+            }
+        }
+    }
+}
+    
