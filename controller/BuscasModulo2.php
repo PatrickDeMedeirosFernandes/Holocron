0fe525133f27e38c06fc15ed6bcca315ece484f8
Patrick <Patrick@192.168.246.57>
2018-01-10 12:34:07 -0200

    atulização Torre de babel modulo 2 e adição a efento na página sobre o holocron

--- controller/BuscasModulo2.php
+++ controller/BuscasModulo2.php
@@ -5,7 +5,7 @@ set_time_limit("1000");
 function BuscaSimilar($text) {
     include '../controller/DB.php';
     $SAIDA = '';
-
+    $i = 0;
     //BUSCA NA PRIEMIRA TABELA
     $sql = "SELECT `id_pergunta`, `pergunta`,`valida` ,u.resposta, id,
      LEVENSHTEIN_RATIO( '$text', `pergunta` ) as textDiff
@@ -13,30 +13,93 @@ function BuscaSimilar($text) {
      LEFT JOIN respota u ON p.resposta_pergunta = u.id 
      HAVING AVG(textDiff) > 70 and max(textDiff) 
      ORDER BY `textDiff` DESC";
-    echo $sql;
+    //echo $sql;
+    /* @var $conn type */
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($linha = $result->fetch_assoc()) {
-            
+            $i++;
             $bb = $linha['id'];
             $aa = $linha['resposta'];
             $SAIDA = $aa;
+            if ($i == 1) {
+                echo "i=".$i;
             $quary4 = "INSERT INTO `perg_user`(`pergunta`, `valida`, `respota_perg_user`) 
                     VALUES('$text',1,'$bb');";
-            echo $quary4;
-
-            if (mysqli_query($conn, $quary4)) {
-                
-            } else {
-                $SAIDA .= "<H1>ESTAMOS COM PROBLEMAS TECNICOS, VOLTE MAIS TARDE</H1>";
+             echo $quary4;
+                if (mysqli_query($conn, $quary4)) {
+                      $SAIDA = " ok".$i;
+                } else {
+                    $SAIDA .= "<H1>ESTAMOS COM PROBLEMAS TECNICOS, VOLTE MAIS TARDE</H1>";
+                }
             }
         }
     } else {
-        
+        $i = 0;
+
+        $sql = "SELECT `id_perg_user`, `pergunta`, `valida`, `data`, u.resposta, id,  LEVENSHTEIN_RATIO( '$text', `pergunta` ) as textDiff
+                FROM `perg_user` p
+                LEFT JOIN respota u ON p.respota_perg_user = u.id
+                HAVING AVG(textDiff) > 70 and max(textDiff) 
+                ORDER BY `textDiff` DESC";
+        $result3 = $conn->query($sql);
+        if ($result3->num_rows > 0) {
+            while ($linha2 = $result3->fetch_assoc()) {
+                $i++;
+                $bb = $linha2['id'];
+                $aa = $linha2['resposta'];
+                $SAIDA = $aa;
+                if ($i == 1) {
+                    $quary4 = "INSERT INTO `perg_user`(`pergunta`, `valida`, `respota_perg_user`) 
+                    VALUES('$text',1,'$bb');";
+                    // echo $quary4;
+
+                    if (mysqli_query($conn, $quary4)) {
+                        //  $SAIDA = " ok";
+                    } else {
+                        $SAIDA .= "<H1>ESTAMOS COM PROBLEMAS TECNICOS, VOLTE MAIS TARDE</H1>";
+                    }
+                }
+            }
+        } else {
+            $i = 0;
+
+//BUSCA NA TERCEIRA TABELA
+            $sql = "SELECT `id_perg_sem_resp`, `pergunta`, `data`, p.resposta as respostaEscrita,  LEVENSHTEIN_RATIO( '$text', `pergunta` ) as textDiff,id,
+                `ip`, u.resposta as respotaReal 
+                 FROM `perg_sem_resp` p
+                 LEFT JOIN respota u ON p.resposta_pergunta = u.id 
+                    where u.resposta is not null or p.resposta is not null 
+                    HAVING AVG(textDiff) > 70 and 
+                    max(textDiff) ORDER BY `textDiff` DESC
+                    ;";
+            $result8 = $conn->query($sql);
+            if ($result8->num_rows > 0) {
+                while ($linha8 = $result8->fetch_assoc()) {
+                    $i++;
+
+                    $aa = $linha8['respotaReal'] . $linha8['respostaEscrita'];
+                    $SAIDA = $aa;
+                    $bb = $linha8['id'];
+                    if ($i == 1) {
+
+                        $quary4 = "INSERT INTO `perg_user`(`pergunta`, `valida`, `respota_perg_user`) 
+                    VALUES('$text',1,'$bb');";
+                        // echo $quary4;
+                        if (mysqli_query($conn, $quary4)) {
+                            //  $SAIDA = " ok";
+                        } else {
+                            $SAIDA .= "<H1>ESTAMOS COM PROBLEMAS TECNICOS, VOLTE MAIS TARDE</H1>";
+                        }
+                    }
+                }
+            } else {
+                $i = 0;
+
+                $SAIDA = ' ';
+            }
+        }
     }
-
-
-
 
 
 
@@ -45,67 +108,6 @@ function BuscaSimilar($text) {
 //https://www.google.com.br/search?ei=4n9TWsC5LoSUwgTb0Iv4Ag&q=wallpaper+full+hd+star+wars&oq=wallpaper+full+hd+st&gs_l=psy-ab.1.1.0l2j0i22i30k1l8.1382.1680.0.3407.2.2.0.0.0.0.189.355.0j2.2.0....0...1c.1.64.psy-ab..0.2.354...0i67k1.0.O4hITv21Jb8
 //https://gist.github.com/Kovah/df90d336478a47d869b9683766cff718
     //https://stackoverflow.com/questions/4671378/levenshtein-mysql-php
-
-    return $SAIDA;
-}
-
-/**
- * ele faz uma analise da frase que vai que foi enviada e busca no banco de dados alguma pergunta que seja igual a pergunta informada,
- *  e enviar uma resposta caso essa tenha no banco de dados
- * @param type $text aqui ele recebe a frase em forma de string
- * @return string retorna a resposta caso tenha no banco de dados senhão envia vazio ==> ''
- */
-function BuscaConcreta5($text) {
-    include '../controller/DB.php';
-    $SAIDA = '';
-
-    //BUSCA NA PRIEMIRA TABELA
-    $sql = "SELECT `id_pergunta`, `pergunta`,`valida` ,u.resposta, id FROM `pergunta` p
-    LEFT JOIN respota u ON p.resposta_pergunta = u.id
-    where p.pergunta = '$text' ;";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while ($linha = $result->fetch_assoc()) {
-            $aa = $linha['resposta'];
-            $SAIDA = $aa;
-        }
-    } else {
-//BUSCA NA SEGUNDA TABALE
-        $sql = "SELECT `id_perg_user`, `pergunta`, `valida`, `data`, u.resposta   
-                FROM `perg_user` p
-                LEFT JOIN respota u ON p.respota_perg_user = u.id
-               where valida = 1 and pergunta = '$text';";
-        $result2 = $conn->query($sql);
-        if ($result2->num_rows > 0) {
-            while ($linha2 = $result2->fetch_assoc()) {
-                $aa = $linha2['resposta'];
-                $SAIDA = $aa;
-            }
-        } else {
-//BUSCA NA TERCEIRA TABELA
-            $sql = "SELECT `id_perg_sem_resp`, `pergunta`, `data`, p.resposta as respostaEscrita, 
-                `ip`, u.resposta as respotaReal 
-                 FROM `perg_sem_resp` p
-                 LEFT JOIN respota u ON p.resposta_pergunta = u.id 
-                    where 
-                    p.pergunta = '$text' and u.resposta is not null
-                     or p.pergunta = '$text' and p.resposta is not null ;";
-            $result2 = $conn->query($sql);
-            if ($result2->num_rows > 0) {
-
-                while ($linha2 = $result2->fetch_assoc()) {
-                    $aa = $linha2['respotaReal'] . $linha2['respostaEscrita'];
-                    $SAIDA = $aa;
-                }
-            } else {
-                $SAIDA = ' ';
-            }
-        }
-
-
-
-        //return ' ';
-    }
 
     return $SAIDA;
 }
--- view/sobreholo.php
+++ view/sobreholo.php
@@ -1,10 +1,279 @@
 <?php include '../includes/cabecalho.php'; ?>
-<div id='bodybox'>
-    <h4>Holocron</h4>
+<!--https://codepen.io/geoffgraham/pen/BpwqOE?depth=everything&order=popularity&page=2&q=star+wars&show_forks=false-->
+<!--<div id='bodybox'>-->
+<!--    <h4>Holocron</h4>
     <p> Holocrom é um assistente virtual criado que conversa com você sobre a franquia de series e filmes
         <a href="Star_Wars">Star Wars</a>, criado por Patrick de Medeiros Fernandes como projeto de conclusão de curso, 
     </p>
-    <br>
-</div>
+    <br>-->
+<!-- I did not find the text of the original generic. So here is a translation into my native language. -->
+
+    <div id="wrapper">
+        <div id="container">
+
+            <div id="paragraph">
+                <h2>Episode V</h2>
+                <h1>The Empire Strikes Back</h1>
+                <p>It is a dark time for the Rebellion. Although the Death Star has been destroyed, Imperial troops have driven the
+                    Rebel forces from their hidden base and pursued them across the galaxy.</p>
+
+                <p>Evading the dreaded Imperial Starfleet, a group of freedom fighters led by Luke Skywalker has established a 
+                    new secret base on the remote ice world of Hoth.</p>
+
+                <p>The evil lord Darth Vader, obsessed with finding young Skywalker, has dispatched thousands of remote 
+                    probes into the far reaches of space...</p>
+            </div>
+        </div>
+    </div>
+
+<style>
+    /*-- general stuff ------------------ */
+    body {
+        background: #000;
+        overflow: hidden;
+        text-align: center;
+        font-size: 16px;
+    }    #wrapper {
+        display: table;
+        width: 100%;
+        height: 100%;
+    }
+    .tableCellDiv {
+        display: table-cell;
+        vertical-align: middle;
+        text-align: center;
+        position: relative;
+        z-index: 999;
+        width: 100%;
+        height: 100vh;
+    }
+    /*-- image ------------------ */
+    #title {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: block;
+        text-align: center;
+        color: #fee71f;
+        z-index: 999;
+    }
+    img {
+        width: 70%;
+        margin-top: 10%;
+    }
+    .playFade {
+        -webkit-animation: fadeAway 8s linear forwards;
+        -moz-animation: fadeAway 8s linear forwards;
+        animation: fadeAway 8s linear forwards;
+    }
+    /*-- text ------------------ */
+    h1 {
+        text-align: center;
+        font-size: 48px;
+    }
+    h2 {
+        text-align: center;
+        font-size: 32px;
+    }
+
+    p {
+        font-size: 32px;
+    }
+
+    .playScroll {
+        -webkit-animation: scroll 70s linear forwards;
+        -moz-animation: scroll 70s linear forwards;
+        animation: scroll 70s linear forwards;
+    }
+
+    #aLongTimeAgo {
+        width: 440px;
+        position: relative;
+        margin: 0 auto;
+        display: block;
+        font-size: 32px;
+        text-align: justify;
+        color: #00ffff;
+        z-index: 999;
+    }
+
+    #container {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        margin: 0 auto;
+        padding: 0;
+        height: 100%;
+        width: 822px;
+        overflow: hidden;
+        text-align: justify;
+        color: #fee71f;
+        transform-origin: 50%;
+        transform: perspective( 600px) rotateX( 50deg);
+    }
+
+    #container:after {
+        content: '';
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: linear-gradient(top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) 100%);
+        background: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) 100%);
+        pointer-events: none;
+    }
+
+    #paragraph {
+        position: relative;
+        top: 100%;
+        border: 1px solid black;
+    }
+    /*-- button ------------------*/
+
+    input[type="button"] {
+        position: relative;
+        display: block;
+        margin: 0 auto;
+        border: solid 2px #fee71f;
+        border-radius: 7px;
+        width: 200px;
+        height: 70px;
+        background: none;
+        color: #fee71f;
+        font-size: 24px;
+        font-weight: bold;
+        cursor: pointer;
+        z-index: 999;
+    }
+    /*-- keyframes ------------------ */
+
+    @-webkit-keyframes fadeAway {
+        0% {
+            opacity: 1;
+            -webkit-transform: scale(1.5) translateY(-0.75em);
+        }
+        100% {
+            opacity: 0;
+            -webkit-transform: scale(0) translateZ(-100em);
+        }
+    }
+
+    @-moz-keyframes fadeAway {
+        0% {
+            opacity: 1;
+            -moz-transform: scale(1.5) translateY(-0.75em);
+        }
+        100% {
+            opacity: 0;
+            -moz-transform: scale(0) translateZ(-100em);
+        }
+    }
+
+    @keyframes fadeAway {
+        0% {
+            opacity: 1;
+            transform: scale(1.5) translateY(-0.75em);
+        }
+        100% {
+            opacity: 0;
+            transform: scale(0) translateZ(-100em);
+        }
+    }
+
+    @-webkit-keyframes scroll {
+        100% {
+            top: -475px;
+        }
+    }
+
+    @-moz-keyframes scroll {
+        100% {
+            top: -475px;
+        }
+    }
+
+    @keyframes scroll {
+        100% {
+            top: -475px;
+        }
+    }</style>
+
+<script type="text/javascript">
+    //variables
+    var $startButton = $('<div class="tableCellDiv"><input type="button" value="Start"/></div>');
+    var $replayButton = $('<div class="tableCellDiv"><input type="button" value="Replay"/></div>');
+    var $starWars = $('<div id="title"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2000px-Star_Wars_Logo.svg.png"/></div>');
+    var $aLongTimeAgo = $('<div class="tableCellDiv"><div id="aLongTimeAgo">A long time ago in a galaxy far, far away...</div></div>');
+    var $crawl = $('#paragraph');
+    var $container = $('#container');
+
+//functions 
+//starts animation
+    function start() {
+        $(this).detach();
+        $("#wrapper").append($container);
+        $aLongTimeAgo.fadeIn(2000).delay(1000).fadeOut(2000).queue(function () {
+            $starWars.show().addClass("playFade");
+            $crawl.show().addClass("playScroll").dequeue();
+        });
+        $crawl.one('webkitAnimationEnd mozanimationend msAnimationEnd animationend',
+                function () {
+                    // code to execute after animation ends
+                    $(this).fadeOut(700);
+                    $replayButton.fadeIn(1000);
+                });
+    }
+
+//replays animation
+    function replay() {
+        $(this).detach();
+        $("#wrapper").append($container);
+        //clone aLongTimeAgo 
+        $replayALongTimeAgo = $aLongTimeAgo.clone(true).remove().appendTo("#wrapper").hide().fadeIn(2000).delay(1000).fadeOut(2000).queue(function () {
+            //clone starWars 
+            $replayStarWars = $starWars.clone(true);
+            $starWars.before($replayStarWars);
+            $("." + $starWars.attr("class") + ":last").remove().appendTo("#wrapper");
+            //clone Crawl
+            $replayCrawl = $crawl.clone(true).show().one('webkitAnimationEnd mozanimationend msAnimationEnd animationend',
+                    function () {
+                        $(this).fadeOut(700);
+                        $replayButton.appendTo("#wrapper").fadeIn(1000);
+                    });
+            $crawl.before($replayCrawl);
+            $("." + $crawl.attr("class") + ":last").remove().appendTo("#container");
+        });
+        $aLongTimeAgo.before($replayALongTimeAgo);
+        $("." + $aLongTimeAgo.attr("class") + ":last").remove().appendTo("#wrapper");
+
+    }
+
+//add a start button
+    $("#wrapper").append($startButton);
+//ass a replay button
+    $("#wrapper").append($replayButton);
+// add star wars title
+    $("#wrapper").append($starWars);
+//add aLongTimeAgo
+    $("#wrapper").append($aLongTimeAgo);
+
+//hide elements
+    $starWars.hide();
+    $crawl.hide();
+    $aLongTimeAgo.hide();
+    $replayButton.hide();
+
+//start button
+    $startButton.on("click", start);
+
+//replay button
+    $replayButton.on("click", replay);
+</script>
+<!--</div>-->
 <?php
 include '../includes/rodape.php';
